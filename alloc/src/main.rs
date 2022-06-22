0d9aa16fc60dd52d8e7b005401fac088c776b5a7
Dmitriy Onypko <vaporif@gmail.com>
2022-06-22 14:19:57 +0100

    upd

--- alloc/src/main.rs
+++ alloc/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let s = String::from("new word");
+    println!("{}", first_word(&s));
 
-    println!("{}, world!", s1);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
