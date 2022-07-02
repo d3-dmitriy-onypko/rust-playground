6b75e1447b284b54c0bd0c0c732886024a9a73de
Dmitriy Onypko <vaporif@gmail.com>
2022-07-02 20:08:11 +0100

    upd

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,6 +17,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn some() {
+    let buffer: &mut [i32] = &mut [1, 2, 3, 4, 5];
+    let coefficients: [i64; 12] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let qlp_shift: i16 = 2;
+
+    for i in 12..buffer.len() {
+        let prediction = coefficients
+            .iter()
+            .zip(&buffer[i - 12..i])
+            .map(|(&c, &s)| c * s as i64)
+            .sum::<i64>()
+            >> qlp_shift;
+        let delta = buffer[i];
+        buffer[i] = prediction as i32 + delta;
+    }
+}
+
 pub struct Config {
     query: String,
     filename: String,
